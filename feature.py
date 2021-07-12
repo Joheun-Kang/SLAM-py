361598dc3a00c14c3dd0ade2811e4211646f50d6
Joheunkang <joheunkang93@gmail.com>
2021-07-11 22:01:37 -0700

    tab errors

--- feature.py
+++ feature.py
@@ -20,77 +20,56 @@ def normalize(K,pt):
 
 class extract_feature(object):
 
-    def __init__(self,K):
-      self.orb = cv2.ORB_create()
-      self.bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck = True)
-      self.last = None 
+  def __init__(self,K):
+    self.orb = cv2.ORB_create()
+    self.bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck = True)
+    self.last = None 
  
-      self.K = K 
-      self.Kinv = np.linalg.inv(self.K)
+    self.K = K 
+    self.Kinv = np.linalg.inv(self.K)
     
-    def normalize(self,K,pt):
-      return np.dot(np.linalg(K),pt)[0:2,:]
+  def normalize(self,K,pt):
+    return np.dot(np.linalg(K),pt)[0:2,:]
       
 
     
-    def extract(self,frame,K):
-      features = cv2.goodFeaturesToTrack(np.mean(frame,axis = 2).astype(np.uint8),3000,qualityLevel = 0.01, minDistance = 2)  
+  def extract(self,frame,K):
+    features = cv2.goodFeaturesToTrack(np.mean(frame,axis = 2).astype(np.uint8),3000,qualityLevel = 0.01, minDistance = 2)  
      
-      kps = [cv2.KeyPoint(x = f[0][0], y = f[0][1], _size = 20) for f in features]
-      kps, des = self.orb.compute(frame,kps)
-
-        if len(good)> 0:
-            good = np.array(good)
-            
-            # normalziation
-            good[:,:,0] -= frame.shape[0]//2
-            good[:,:,1] -= frame.shape[1]//2
-            
-            model,inliers = ransac((good[:,0],good[:,1]),
-                FundamentalMatrixTransform,
-                min_samples = 8,
-                residual_threshold = 1,
-                max_trials = 120)
-
-            good = good[inliers]
-
-            #only good mache about 700
-            print('mached:',len(good))
-
-
-
-      matches = None
-      matching_pts = []
+    kps = [cv2.KeyPoint(x = f[0][0], y = f[0][1], _size = 20) for f in features]
+    kps, des = self.orb.compute(frame,kps)
+    
+    matches = None
+    matching_pts = []
         
 
-      # find matching points 
-      bf = cv2.BFMatcher()
-      if self.last != None:
-        matches = bf.knnMatch(des,self.last['des'],k=2)
+    # find matching points 
+    bf = cv2.BFMatcher()
+    if self.last != None:
+      matches = bf.knnMatch(des,self.last['des'],k=2)
         
-        for m,n in matches:
-          if m.distance < 0.75*n.distance:
-            kp1 = kps[m.queryIdx].pt
-            kp2 = self.last['kps'][m.trainIdx].pt 
-            matching_pts.append((kp1, kp2))
+      for m,n in matches:
+        if m.distance < 0.75*n.distance:
+          kp1 = kps[m.queryIdx].pt
+          kp2 = self.last['kps'][m.trainIdx].pt 
+          matching_pts.append((kp1, kp2))
         
-      if len(matching_pts)>0:
-        matching_pts = np.array(matching_pts)
+    if len(matching_pts)>0:
+      matching_pts = np.array(matching_pts)
 
-        # with the matching points, we find the "keymatching" points
-        # from this ransac, we find the homography
-        model,inliers = ransac((matching_pts[:,0],matching_pts[:,1]),FundamentalMatrixTransform,min_samples = 8,residual_threshold = 1,max_trials = 120)
+      # with the matching points, we find the "keymatching" points
+      # from this ransac, we find the homography
+      model,inliers = ransac((matching_pts[:,0],matching_pts[:,1]),FundamentalMatrixTransform,min_samples = 8,residual_threshold = 1,max_trials = 120)
           
-        # this is better matching 'key' points
-        matching_pts= matching_pts[inliers]
+      # this is better matching 'key' points
+      matching_pts= matching_pts[inliers]
 
-        return np.array(good)
 
         
 
-      self.last = {'kps':kps , 'des':des}
+    self.last = {'kps':kps , 'des':des}
         
-      return kps,np.array(matching_pts)
+    return kps,np.array(matching_pts)
        
 
 
