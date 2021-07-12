30b44cbd57b3ab5eba2a71f2e87405707ef640f7
Joheunkang <joheunkang93@gmail.com>
2021-07-12 01:28:42 -0700

    hmg+normalize

--- slam.py
+++ slam.py
@@ -4,7 +4,7 @@ import pygame
 import sys
 import skimage
 from pygame.locals import DOUBLEBUF
-from feature import extract_feature, create_hmg, normalize
+from feature import extract_feature
 
 from display import Display2D
 
@@ -19,22 +19,31 @@ K = np.array([[F,0,w//2],[0,F,h//2],[0,0,1]])
 
 fe = extract_feature(K)
 
+def create_hmg(pts):
+  n = pts.shape[0]
+  return np.concatenate([pts,np.ones(1)],axis= 0)
 
+def normalize(K,pt):
+  # modeul eror : we can not return and use normalized , need to define one varialbe 
+  normalized = np.dot(np.linalg.inv(K),pt)
+  return normalized
 
 def image_process(frame):
   # get goot match points (not normalized )
   kps, matching_pts= fe.extract(frame,K)
 
-  print(matching_pts)
   
   
   for pts1, pts2 in matching_pts:
     
-    print(pts1)
-    #print(pts1.shape)#(2,)
-    pts1_n = create_hmg(pts1)
-    print(pts1_n)
-    print('normalized pts1_n',pts1_n.shape)
+    # add hmg + normalize with inverse(intrinsic) maxtix mul
+    pts1_hmg = create_hmg(pts1)
+    pts2_hmg = create_hmg(pts2)
+
+    # noramlized points 
+    pts1_n = normalize(K,pts1_hmg)[:2]
+    pts2_n = normalize(K,pts2_hmg)[:2]
+  
     u1,v1 = map(lambda x: int(round(x)),pts1)
     u2,v2 = map(lambda x: int(round(x)),pts2)
   
